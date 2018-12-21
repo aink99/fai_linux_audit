de880f66c994cbe289ce92577ade949c3e6b147f
Sebastien Chartrand <sebastien.chartrand@fujitsu.com>
2018-12-21 16:58:10 -0500

    Added case for oracle version

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,12 +2,22 @@ default['audit']['reporter'] = 'chef-server-automate'
 default['audit']['fetcher'] = 'chef-server'
 case node['platform']
 when 'oracle'
-  default['audit']['profiles'] = [
-    {
-      'name' => 'linux',
-      'compliance' => 'audit/cis-oraclelinux7-level1',
-    },
-  ]
+  case node['platform_version']
+  when /^7/
+    default['audit']['profiles'] = [
+      {
+        'name' => 'linux',
+        'compliance' => 'audit/cis-oraclelinux7-level1',
+      },
+    ]
+  when /^6/
+    default['audit']['profiles'] = [
+      {
+        'name' => 'linux',
+        'compliance' => 'audit/cis-rhel6-level1-server',
+      },
+    ]
+  end
 when 'redhat'
   default['audit']['profiles'] = [
     {
@@ -21,6 +31,6 @@ end
 
 
 #   "platform": "oracle",
-  #  "platform_version": "7.5",
+  #  "platform_version": "/^7./",
   #  "platform_family": "rhel",
   #  "hostnamectl": {
